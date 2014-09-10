d68e6ba733653070e4edc1fb2b422420457b78c4
Sara Collini <scolli16@masonlive.gmu.edu>
2014-09-10 11:42:43 -0400

    Adding recursive

---
+++ recursive.js
@@ -0,0 +1,13 @@
+/* Originally, I made this simple function */
+
+function isEven(target) {
+  if(target % 2 == 0)
+    return true;
+  else
+    return false;
+}
+console.log(isEven(-2)); // But it's not recursive...I need more help with this problem
+
+/* I tried to implement the || symbol, because it would return to the left when true and 
+return to right otherwise. */
+
