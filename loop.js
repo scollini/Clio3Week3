0dff37f690e0a57648ee5dcc0e531b4b81974db7
Sara Collini <scolli16@masonlive.gmu.edu>
2014-09-10 11:39:29 -0400

    Adding loop exercise

---
+++ loop.js
@@ -0,0 +1,5 @@
+
+/* Looping a Triangle- I used the example, like the author suggested, and replaced the output numbers with the "#"s */
+
+for (var number = "#"; number.length <= 7; number = number += "#")
+  console.log(number);
