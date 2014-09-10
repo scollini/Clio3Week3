65f636c8f6925a1c51d0004cdb7287bd5aea544a
Sara Collini <scolli16@masonlive.gmu.edu>
2014-09-10 11:40:18 -0400

    Adding grid

---
+++ grid.js
@@ -0,0 +1,10 @@
+/* This is how far I got before I looked at the solution, and it was still confusing. I somehow got it to repeat the pattern for a loooong time, but could not get it to repeat just 8 times or for any number of times. */
+
+var line = "# # # #\n # # # #";
+console.log(line);
+
+for (var size = 0; size <= 8; size +1) //I tried defining size, but I didn't get anywhere
+
+for (var x = 0;; x < 10; x ++) {
+	for (var x = 0; x < 10;) // I also tried to define an x and a y for the grid
+}
