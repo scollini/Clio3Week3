90956d64636c6f87053dc7bca518ce8db335199f
Sara Collini <scolli16@masonlive.gmu.edu>
2014-09-10 11:40:37 -0400

    Adding min exercise

---
+++ min.js
@@ -0,0 +1,11 @@
+/* Minimum- did this one by myself! I thought this problem was the easiest, because it 
+only looked like a math problem */
+
+function min(x, y) {
+  if (x <= y)
+    return x;
+  else if (y <= x)
+    return y;
+}
+console.log(min(3, 2));
+
